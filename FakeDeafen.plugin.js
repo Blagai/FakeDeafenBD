93c9dd83938fb5f6d6597f8b7447847151324050
Chloe <AyEmREL@protonmail.com>
2023-06-27 23:11:20 +0300

    Update FakeDeafen.plugin.js
    I still can't figure out how to change the Trigger key but at least I got somewhat closer, the button works now. I hate my life and I hate JS even more.

--- FakeDeafen.plugin.js
+++ FakeDeafen.plugin.js
@@ -1,108 +1,124 @@
 /**
-* @name FakeDeafen
-* @author ChloeTheBitch
-* @description Lets you appear deafened while still being able to hear and talk
-* @version 0.0.4
-* @source https://github.com/ChloeTheBitch/FakeDeafenBD
-* @donate https://paypal.me/ArielChloeMann
-*/
+ * @name FakeDeafen
+ * @author ChloeTheBitch
+ * @description Lets you appear deafened while still being able to hear and talk
+ * @version 0.0.4
+ * @source https://github.com/ChloeTheBitch/FakeDeafenBD
+ * @donate https://paypal.me/ArielChloeMann
+ */
 
-// todo make the settings thingy work (thingy being panel and the settings inside lmao)
+// todo make the setting button do something
 // todo make a ui button to trigger it as well as keyboard shortcut
 // todo make it possible to turn off without restarting Discord
 
-module.exports = class fakeDeafen
-{
-	constructor(meta)
+module.exports = class FakeDeafen {
+	
+	// What the hell kind of coding language doesn't have the stuff defined in the constructor be publicly accesible?
+	// Well the answer is JavaScript, so now I have to make the plugin look awful to make it work
+	// Fuck you JS
+    constructor(meta) 
 	{
-		// Set up the settings
-		const mySettings = {triggerKey: "d"};
-		
-		// The actual code that does everything
-		function trigger()
-		{
-			var glob = new TextDecoder("utf-8");
-			WebSocket.prototype.original = WebSocket.prototype.send;
-			WebSocket.prototype.send = function(data)
+        this.meta = meta;
+        this.mySettings = { triggerKey: "d" };
+    }
+
+	// Main code
+    trigger() 
+	{
+        const glob = new TextDecoder("utf-8");
+        WebSocket.prototype.original = WebSocket.prototype.send;
+        WebSocket.prototype.send = function (data) {
+            if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") 
 			{
-				if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") 
+                if (glob.decode(data).includes("self_deaf")) 
 				{
-					if (glob.decode(data).includes("self_deaf"))
-					{
-						data = data.replace('"self_mute":false');
-					}
-				}
-				WebSocket.prototype.original.apply(this, [data]);
-			}
-			window.BdApi.alert("success",`If you want to stop the plugin, restart Discord (CTRL+R)`);
-		}
-		
-		// Function to update the trigger key. Don't think it works but I can't even check because the settings panel won't open lmao
-		function updateTriggerKey()
-		{
-			window.BdApi.alert ("Func called");
-		}
-	}
-	
-	start()
+                    data = data.replace('"self_mute":false');
+                }
+            }
+            WebSocket.prototype.original.apply(this, [data]);
+        };
+        window.BdApi.alert("success", "If you want to stop the plugin, restart Discord (CTRL+R)");
+    }
+
+	// Func that gets called when button in settings is pressed
+	// Doesn't do anyhthing important rn
+    updateTriggerKey() 
 	{
-		// Check if required library downloaded and download if not
-		if (!global.ZeresPluginLibrary)
-		{
-			BdApi.UI.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-			{
-				confirmText: "Download Now",
-				cancelText: "Cancel",
-				onConfirm: () => 
-				{
-					require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) =>
-					{
-						if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
-						await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-					});
-				}
-			});
-		}
-		
-		// Trigger the plugin when ctrl + triggerKey are pressed
-		document.addEventListener("keydown", function(event)
-		{
-			if (event.ctrlKey && event.key === triggerKey)
-			{
-				trigger();
-			}
-		
-		});
-	}
-	
-	stop()
+        window.BdApi.alert("Func called");
+    }
+
+    start() 
 	{
-		
+		// Check if required library is downloaded and prompt the user to download it if it isn't
+        if (!global.ZeresPluginLibrary) 
+		{
+            BdApi.UI.showConfirmationModal(
+                "Library Missing",
+                `The library plugin needed for ${this.meta.name} is missing. Please click Download Now to install it.`,
+                {
+                    confirmText: "Download Now",
+                    cancelText: "Cancel",
+                    onConfirm: () => {
+                        require("request").get(
+                            "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                            async (error, response, body) => {
+                                if (error)
+                                    return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                                await new Promise((r) =>
+                                    require("fs").writeFile(
+                                        require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"),
+                                        body,
+                                        r
+                                    )
+                                );
+                            }
+                        );
+                    },
+                }
+            );
+        }
+
+		// What's used to trigger the main code
+        document.addEventListener("keydown", (event) => 
+		{
+            if (event.ctrlKey && event.key === this.mySettings.triggerKey) 
+			{
+                this.trigger();
+            }
+        });
+    }
+
+    stop() 
+	{
+		// This is sad
 	}
-	
+
 	// Create the settings panel
-	// Does not work at all lol
-	getSettingsPanel()
+	// Finally works correctly
+    getSettingsPanel() 
 	{
-		const mySettingsPanel = document.createElement("div");
+        const mySettingsPanel = document.createElement("div");
         mySettingsPanel.id = "my-settings";
 
         const triggerKeySetting = document.createElement("div");
         triggerKeySetting.classList.add("setting");
 
-        const triggerKeySettingLabel = document.createElement("span")
+        const triggerKeySettingLabel = document.createElement("span");
         triggerKeySettingLabel.textContent = "Trigger keyboard shortcut";
 
+		// Create the button that triggers the function that takes user input and updates the trigger key to it. Caused me so much pain.
         const triggerKeySettingButton = document.createElement("button");
-		triggerKeySettingButton.textContent = "Change Key";
-		// Why tf is this not working?! JavaScript is fucking trash istg
-		triggerKeySettingButton.addEventListener("click", this.updateTriggerKey);
+        triggerKeySettingButton.textContent = "Change Key";
+        triggerKeySettingButton.addEventListener("click", () => 
+		{
+            this.updateTriggerKey();
+        });
 
         triggerKeySetting.appendChild(triggerKeySettingLabel);
-		triggerKeySetting.appendChild(triggerKeySettingButton);
+        triggerKeySetting.appendChild(triggerKeySettingButton);
 
         mySettingsPanel.append(triggerKeySetting);
-		
+
         return mySettingsPanel;
-	}
-}
+    }
+};
