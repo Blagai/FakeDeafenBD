cbad0b4e68c2765576588b01926fcd9341e4b805
Chloe <AyEmREL@protonmail.com>
2023-06-28 14:51:27 +0300

    I'm dumb

--- FakeDeafen.plugin.js
+++ FakeDeafen.plugin.js
@@ -2,7 +2,7 @@
  * @name FakeDeafen
  * @author ChloeTheBitch
  * @description Lets you appear deafened while still being able to hear and talk
- * @version 0.1.0
+ * @version 0.1.2
  * @source https://github.com/ChloeTheBitch/FakeDeafenBD
  * @donate https://paypal.me/ArielChloeMann
  */
